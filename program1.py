0005cabf9daa97a0fcefbce8e52ee9bce5506f2d
sumit-19-bit <0112cs211124@gmail.com>
2024-09-25 13:55:30 +0530

    run

--- program1.py
+++ program1.py
@@ -1,6 +1,23 @@
 class Solution:
-   
     def getTotalIsles(self, grid: list[list[str]]) -> int:
-    #    write your code here
-                    
-        return 0
+        if not grid or not grid[0]:
+            return 0
+        
+        rows, cols = len(grid), len(grid[0])
+        directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+        island_count = 0
+
+        def dfs(r, c):
+            grid[r][c] = 'W'  # Mark the cell as water
+            for dr, dc in directions:
+                nr, nc = r + dr, c + dc
+                if 0 <= nr < rows and 0 <= nc < cols and grid[nr][nc] == 'L':
+                    dfs(nr, nc)
+
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == 'L':  # Found an island
+                    island_count += 1
+                    dfs(r, c)  # Explore the entire island
+
+        return island_count
